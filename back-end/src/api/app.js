3e8a9d6c1d6e9c9832e9538278eba11c074b9002
Jeferson Oliveira <jeferson.contas@gmail.com>
2022-02-22 13:59:07 -0300

    criacao pastas

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
