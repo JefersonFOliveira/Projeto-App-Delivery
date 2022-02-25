04ea7303dec0746b1e0b51d7c835207f411d2296
Miguel Dantas <miiguelalb@gmail.com>
2022-02-25 13:25:31 -0300

    feat: Iniciando a página de registrar novo usuário

--- front-end/src/pages/registration/registration.js
+++ front-end/src/pages/registration/registration.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [buttonDisabled, enbaleButton] = useState(true);
+  const [logged, setLogged] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const regex = /[a-z0-9]+@[a-z]+\.[a-z]{3}/;
+    const minLength = 6;
+    const nameLength = 12;
+
+    if(password.length >= minLength && regex.test(email) && name.length >= nameLength) {
+      enbaleButton(false);
+    } else {
+      enbaleButton(true);
+    }
+  }, [password, email, name]);
+
   return (
     <div>Registrar</div>
   );
