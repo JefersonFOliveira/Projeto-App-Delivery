44735d8ef71ee0b8a3ebb491b34527e233d0dd2f
Heder Waine <hederw@hotmail.com>
2022-03-02 19:59:06 -0300

    fix: linter

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,46 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import getLocalStorage from '../helpers/getStorage';
-import './header.css'
+import './header.css';
 
 function Header() {
   const { name } = getLocalStorage('user');
 
   const logout = () => {
     localStorage.removeItem('user');
-  }
+  };
+
   return (
     <header>
       <nav>
-          <Link 
-            to="/customer/products"
-            className="products"
-            data-testid="customer_products__element-navbar-link-products"
-          >
-            PRODUTOS
-          </Link>
-          <Link 
-            to="/customer/orders"
-            className="orders"
-            data-testid="customer_products__element-navbar-link-orders"
-          >
-            MEUS PEDIDOS
-          </Link>
-        </nav>
-        <p
-          className="name"
-          data-testid="customer_products__element-navbar-user-full-name"
+        <Link
+          to="/customer/products"
+          className="products"
+          data-testid="customer_products__element-navbar-link-products"
         >
-          { name }
-        </p>
-        <Link 
-          to="/login"
-          className="logout"
-          data-testid="customer_products__element-navbar-link-logout"
-          onClick={ logout }
-        >
-          Sair
+          PRODUTOS
         </Link>
+        <Link
+          to="/customer/orders"
+          className="orders"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          MEUS PEDIDOS
+        </Link>
+      </nav>
+      <p
+        className="name"
+        data-testid="customer_products__element-navbar-user-full-name"
+      >
+        { name }
+      </p>
+      <Link
+        to="/login"
+        className="logout"
+        data-testid="customer_products__element-navbar-link-logout"
+        onClick={ logout }
+      >
+        Sair
+      </Link>
     </header>
   );
 }
