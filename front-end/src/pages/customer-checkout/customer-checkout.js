aa33f738e568c77b9bcb72991b37c3da392adc9a
Miguel Dantas <miiguelalb@gmail.com>
2022-03-06 13:42:20 -0300

    feat: HTML da página Checkout criado

---
+++ front-end/src/pages/customer-checkout/customer-checkout.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+const fakecheckouts = [
+  {
+    item: 1,
+    name: 'Coca-Cola',
+    quantity: 1,
+    price: 2.50,
+    total: 10.50,
+  },
+  {
+    item: 1,
+    name: 'Pepsi-cola',
+    quantity: 3,
+    price: 13.50,
+    total: 23.32,
+  },
+];
+
+function CustomerCheckout() {
+  const dataTestName = 'customer_checkout__element-order-table-';
+  const total = fakecheckouts.reduce((acc, curr) => acc + curr.total, 0);
+  const handleButtonCheckout = () => {
+    alert('Checkout Successful');
+  };
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Finalizar pedido</h1>
+        <table className="checkout-container">
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+            <th>Remover Item</th>
+          </tr>
+          {fakecheckouts.map((checkout, i) => (
+            <tr key={ i }>
+              <td
+                data-testid={ `${dataTestName}item-number-${i}` }
+              >
+                {i + 1}
+              </td>
+              <td
+                data-testid={ `${dataTestName}name-${i}` }
+              >
+                {checkout.name}
+              </td>
+              <td
+                data-testid={ `${dataTestName}quantity-${i}` }
+              >
+                {checkout.quantity}
+              </td>
+              <td
+                data-testid={ `${dataTestName}unit-price-${i}` }
+              >
+                {checkout.price}
+              </td>
+              <td
+                data-testid={ `${dataTestName}sub-total-${i}` }
+              >
+                {checkout.total}
+              </td>
+              <td
+                data-testid={ `${dataTestName}remove-${i}` }
+              >
+                <button type="button">Remover</button>
+              </td>
+            </tr>
+          ))}
+        </table>
+        <h2
+          data-testid="customer_checkout__element-order-total-price"
+        >
+          { `Total: R$ ${total}` }
+        </h2>
+      </main>
+      <form>
+        <label htmlFor="seller">
+          P. Vendedora Responsável
+          <input
+            type="text"
+            id="seller"
+            data-testid="customer_checkout__select-seller"
+          />
+        </label>
+        <label htmlFor="address">
+          Endereço
+          <input
+            type="text"
+            id="address"
+            data-testid="customer_checkout__input-address"
+          />
+        </label>
+        <label htmlFor="number">
+          Número
+          <input
+            type="text"
+            id="number"
+            data-testid="customer_checkout__input-addressNumber"
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="customer_checkout__button-submit-order"
+          onClick={ handleButtonCheckout }
+        >
+          Finalizar pedido
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CustomerCheckout;
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/login/Login';
 import Register from '../pages/registration/registration';
 import Products from '../pages/products/Products';
+import Checkout from '../pages/customer-checkout/customer-checkout';
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
       <Route exact path="/customer/products" element={ <Products /> } />
       <Route exact path="/register" element={ <Register /> } />
       <Route exact path="/" element={ <Navigate to="/login" /> } />
+      <Route exact path="/customer/checkout" element={ <Checkout /> } />
     </Routes>
   );
 }
